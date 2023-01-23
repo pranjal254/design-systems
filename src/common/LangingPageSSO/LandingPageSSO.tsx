d1823dd8a8add15bab54c149a89e31a2615ae2ac
Pranjal Gaikwad <pranjal.gaikwad@tredence.com>
2023-01-23 19:41:14 +0530

    landing page Bg image

--- src/common/LangingPageSSO/LandingPageSSO.tsx
+++ src/common/LangingPageSSO/LandingPageSSO.tsx
@@ -10,60 +10,76 @@ interface LandingPageSSOProps {
 
 const LandingPageSSO = (props: LandingPageSSOProps) => {
   return (
- 
-      <Container fluid style={{ backgroundColor: "#FFFFFF", backgroundImage:`url(${ssologinImg})`, minHeight:'100vh', padding:'0' }}>
-        <Card
-          style={{
-            float: "left",
-            position: "absolute",
-            left: "55vw",
-            top: "35vh",
-            zIndex: "1000",
-            padding: "5px",
-            fontWeight: "bold",
-            width: "25vw",
-            height: "50vh",
-            background: "rgba(255, 255, 255, 0.2)",
-            border: "1px solid #E5EEFF",
-            boxShadow: "4px 4px 16px rgba(146, 148, 151, 0.25)",
-            backdropFilter: "blur(30px)",
-            borderRadius: "4px",
-          }}
-        >
-          <CardBody>
-            <CardTitle
-              style={{
-                fontSize: "4rem",
-                fontWeight: "600",
-                color: "#FE5000",
-                textAlign: "center",
-                paddingTop: "2rem",
-              }}
-            >
-              {props.projectName}
-            </CardTitle>
-            <div style={{padding:'2rem', marginTop:'5vh'}}>
+    <Container
+      fluid
+      style={{
+        backgroundImage: `url(${ssologinImg})`,
+        padding: "0",
+        height: "100vh",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Card
+        style={{
+          float: "left",
+          position: "absolute",
+          left: "54vw",
+          top: "35vh",
+          zIndex: "1000",
+          padding: "5px",
+          fontWeight: "bold",
+          width: "25vw",
+          height: "50vh",
+          background: "rgba(255, 255, 255, 0.2)",
+          border: "1px solid #E5EEFF",
+          boxShadow: "4px 4px 16px rgba(146, 148, 151, 0.25)",
+          backdropFilter: "blur(30px)",
+          borderRadius: "4px",
+        }}
+      >
+        <CardBody>
+          <CardTitle
+            style={{
+              fontSize: "4rem",
+              fontWeight: "600",
+              color: "#FE5000",
+              textAlign: "center",
+              paddingTop: "2rem",
+            }}
+          >
+            {props.projectName}
+          </CardTitle>
+          <div style={{ padding: "2rem", marginTop: "5vh" }}>
             <span style={{ fontSize: "1rem" }}>
               Sign in using Microsoft credentials
             </span>
-            <div style={{marginBottom:'9vh', marginTop:'1vh'  }}>
+            <div style={{ marginBottom: "9vh", marginTop: "1vh" }}>
               <button
                 className="tre-btn primary md"
-                style={{ fontSize: "1rem", width:"100%" }}
+                style={{ fontSize: "1rem", width: "100%" }}
                 type="submit"
                 onClick={props.handleRedirect}
               >
-                <span style={{marginRight:'1rem'}}>SIGN IN</span>
+                <span style={{ marginRight: "1rem" }}>SIGN IN</span>
                 <img src={microsoft} />
               </button>
             </div>
-            </div>
-          </CardBody>
-          <CardFooter style={{ fontSize: "0.8rem", textAlign: "left", fontWeight:"400", paddingLeft:"2rem" }}>
-            © Copyright Tredence 2022
-          </CardFooter>
-        </Card>
-        {/* <img
+          </div>
+        </CardBody>
+        <CardFooter
+          style={{
+            fontSize: "0.8rem",
+            textAlign: "left",
+            fontWeight: "400",
+            paddingLeft: "2rem",
+          }}
+        >
+          © Copyright Tredence 2022
+        </CardFooter>
+      </Card>
+      {/* <img
           src={ssologinImg}
           alt=""
           className="img-fluid"
@@ -74,9 +90,7 @@ const LandingPageSSO = (props: LandingPageSSOProps) => {
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
           }} */}
-       
-      </Container>
-   
+    </Container>
   );
 };
 
